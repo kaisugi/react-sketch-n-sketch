450bc4ab7ef1b1f1696134332e664469f7541eb5
7ma7X <hellorusk1998@gmail.com>
2020-04-24 23:02:12 +0900

    initialize editor and svg frame

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import AceEditor from "react-ace";
+import "ace-builds/src-noconflict/mode-java";
+import "ace-builds/src-noconflict/theme-github";
+import "ace-builds/src-noconflict/theme-monokai";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="wrapper">
+        <AceEditor
+          mode="javascript"
+          theme="monokai"
+          name="UNIQUE_ID_OF_DIV"
+          width="50%"
+          height="100vh"
+          fontSize={20}
+          editorProps={{ $blockScrolling: true }}
+          value={`function onLoad(editor) {
+  console.log("i've loaded");
+}`}
+        />
+        <svg width="500" height="500" >
+          <line x1="10" y1="10" x2="300" y2="300" style={{stroke:"rgb(255,0,0)",strokeWidth:2}} />
+        </svg>
+      </div>
     </div>
   );
 }
