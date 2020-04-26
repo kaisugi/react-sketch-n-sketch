1907869090e9b710fad3324f02357129447ce872
7ma7X <hellorusk1998@gmail.com>
2020-04-26 16:43:57 +0900

    move line by click

--- src/App.js
+++ src/App.js
@@ -4,38 +4,75 @@ import { Parser } from 'acorn';
 import Editor from './components/editor';
 import renderFigureData from './components/renderFigureData';
 
+
+
 function App() {
-  const [program, setProgram] = useState("line1 = line([153, 118], [421, 234]);");
+  const [program, setProgram] = useState("line1 = line([100, 100], [200, 200]);");
   const [errorMessage, setErrorMessage] = useState("");
-  const [SVGComponent, setSVGComponent] = useState(null);
+  const [SVGComponent, setSVGComponent] = useState(null); // 描画する SVG の Component
+  const [pointsToPos, setPointsToPos] = useState(null); // 点の座標からプログラム上の位置へのマップ
 
   useEffect(() => {
     try {
-      const programNodes = Parser.parse(program);
-
       const newSVGComponent = [];
+      const newPointsToPos = [];
+
+
+      const programNodes = Parser.parse(program);
 
       for (const node of programNodes.body) {
         const data = renderFigureData(node);
 
         if (data.type === "line") {
+          newPointsToPos.push([[Number(data.p1[0]), Number(data.p1[1])], [data.p1Start, data.p1End]]);
+          newPointsToPos.push([[Number(data.p2[0]), Number(data.p2[1])], [data.p2Start, data.p2End]]);
+
           newSVGComponent.push(
-            <line x1={data.p1[0]} y1={data.p1[1]} x2={data.p2[0]} y2={data.p2[1]} stroke="black" key={data.name}/>
+            <line x1={data.p1[0]} y1={data.p1[1]} x2={data.p2[0]} y2={data.p2[1]} stroke="red" key={data.name}/>
           )
         }
       }
 
-      setSVGComponent(newSVGComponent);
+
       setErrorMessage("");
+      setSVGComponent(newSVGComponent);
+      setPointsToPos(newPointsToPos);
+
+      console.log(newPointsToPos)
     } catch (e) {
-      setSVGComponent(null);
       setErrorMessage(`${e}`);
+      setSVGComponent(null);
+      setPointsToPos(null);
     }
   }, [program]);
 
   const handleClick = (e) => {
     const offset = e.target.getBoundingClientRect()
-    // console.log({x: e.clientX - offset.x, y: e.clientY - offset.y})
+
+    const currentX = e.clientX - offset.x;
+    const currentY = e.clientY - offset.y;
+    
+    let min = 100000000000000;
+    let newX = null;
+    let newY = null;
+    let start = null;
+    let end = null;
+
+    for (const m of pointsToPos) {
+      const tmp = (currentX - m[0][0]) * (currentX - m[0][0]) + (currentY - m[0][1]) * (currentY - m[0][1]);
+      if (tmp < min) {
+        min = tmp;
+        newX = currentX;
+        newY = currentY;
+        start = m[1][0];
+        end = m[1][1];
+      }
+    }
+
+    const front = program.slice(0, start);
+    const back = program.slice(end);
+    setProgram(`${front}[${newX}, ${newY}]${back}`);
+
   }
 
   return (
--- src/components/renderFigureData.js
+++ src/components/renderFigureData.js
@@ -1,5 +1,7 @@
 export default (node) => {
 
+  console.log(node)
+
   if (node.type === "ExpressionStatement") {
     const exp = node.expression;
 
@@ -8,7 +10,11 @@ export default (node) => {
         type: "line",
         name: exp.left.name,
         p1: exp.right.arguments[0].elements.map(n => n.raw),
-        p2: exp.right.arguments[1].elements.map(n => n.raw)
+        p1Start: exp.right.arguments[0].start,
+        p1End: exp.right.arguments[0].end,
+        p2: exp.right.arguments[1].elements.map(n => n.raw),
+        p2Start: exp.right.arguments[1].start,
+        p2End: exp.right.arguments[1].end
       }
     }
   }
